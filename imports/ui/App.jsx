44daf0e706fa829d839810ce086cedcb225ff531
Kinza Kasher <32021945+kinzakasher@users.noreply.github.com>
2017-12-10 14:50:13 -0500

    Update App.jsx

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 import P5Wrapper from 'react-p5-wrapper';
 import sketch from './p5/sketch.js';
 import { withTracker } from 'meteor/react-meteor-data';
-import { Ascii } from '../api/ascii.js';
+import { Fishtank } from '../api/fishtank.js';
 
 
 class App extends Component {
@@ -12,7 +12,7 @@ class App extends Component {
     super(props);
 
     this.state = {
-      ascii: {},
+      fishtank: {},
     };
  
   }
@@ -24,50 +24,25 @@ class App extends Component {
           <h1>super-serial-app</h1>
         </header>
         {/*pass the p5 sktech file into the React wrapper
-        also pass the ascii prop which will updated based on withTracker below*/}
-        <P5Wrapper sketch={sketch} ascii={this.props.ascii} />
+        also pass the fishtank prop which will updated based on withTracker below*/}
+        <P5Wrapper sketch={sketch} fishtank={this.props.fishtank} />
       </div>
     );
   }
 }
 
 App.defaultProps = {
-  ascii: {text:"."},
+  fishtank: {text:"."},
 };
 
 App.propTypes = {
-  ascii: PropTypes.object.isRequired,
+  fishtank: PropTypes.object.isRequired,
 };
 
 export default withTracker(props => {
-  Meteor.subscribe('ascii');
+  Meteor.subscribe('fishtank');
   return {
-    ascii: Ascii.find({}, { sort: { updatedAt: -1 } }).fetch()[0],
+    fishtank: Fishtank.find({}, { sort: { updatedAt: -1 } }).fetch()[0],
   };
 })(App);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
